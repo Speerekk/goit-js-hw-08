0cdec323171ed0274b37584a71bf3b404662de9f
Vladislav <80133226+Speerekk@users.noreply.github.com>
2023-05-16 22:53:24 +0200

    hw1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,38 @@
-// Add imports above this line
-import { galleryItems } from './gallery-items';
-// Change code below this line
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+import { galleryItems } from './gallery-items.js';
+
+const galleryList = document.querySelector('.gallery');
+
+const createGalleryItem = item => {
+  const listItem = document.createElement('li');
+  listItem.classList.add('gallery__item');
+
+  const link = document.createElement('a');
+  link.classList.add('gallery__link');
+  link.href = item.original;
+
+  const image = document.createElement('img');
+  image.classList.add('gallery__image');
+  image.src = item.preview;
+  image.alt = item.description;
+
+  link.appendChild(image);
+  listItem.appendChild(link);
+
+  return listItem;
+};
+
+const renderGallery = items => {
+  const galleryItems = items.map(item => createGalleryItem(item));
+  galleryList.append(...galleryItems);
+};
+
+renderGallery(galleryItems);
+
+const lightbox = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+});
 
 console.log(galleryItems);
