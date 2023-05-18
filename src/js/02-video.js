b74558482ef67d4da1514ec71da27cfc3889113b
Vladislav <80133226+Speerekk@users.noreply.github.com>
2023-05-18 14:29:27 +0200

    hw8

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,24 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+// Создаем экземпляр плеера Vimeo
+const vimeoPlayer = new Player('vimeo-player');
+
+// Функция для сохранения текущего времени воспроизведения в локальное хранилище
+const saveCurrentTime = throttle(time => {
+  localStorage.setItem('videoplayer-current-time', time);
+}, 1000); // Сохраняем время не чаще чем раз в секунду
+
+// Обработчик события timeupdate - обновление времени воспроизведения
+vimeoPlayer.on('timeupdate', event => {
+  const currentTime = event.seconds;
+  saveCurrentTime(currentTime);
+});
+
+// Получаем сохраненное время из локального хранилища
+const savedTime = localStorage.getItem('videoplayer-current-time');
+
+// Если есть сохраненное время, устанавливаем его в плеере
+if (savedTime) {
+  vimeoPlayer.setCurrentTime(savedTime);
+}
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,46 @@
+import throttle from 'lodash.throttle';
 
+const form = document.querySelector('.feedback-form');
+const emailInput = form.querySelector('input[name="email"]');
+const messageInput = form.querySelector('textarea[name="message"]');
+const storageKey = 'feedback-form-state';
+
+// Функция сохранения состояния формы в локальное хранилище
+const saveFormState = () => {
+  const state = {
+    email: emailInput.value,
+    message: messageInput.value,
+  };
+  localStorage.setItem(storageKey, JSON.stringify(state));
+};
+
+// Функция загрузки состояния формы из локального хранилища
+const loadFormState = () => {
+  const savedState = localStorage.getItem(storageKey);
+  if (savedState) {
+    const state = JSON.parse(savedState);
+    emailInput.value = state.email;
+    messageInput.value = state.message;
+  }
+};
+
+// Функция обработки сабмита формы
+const handleSubmit = event => {
+  event.preventDefault();
+  console.log('Form submitted');
+  const state = {
+    email: emailInput.value,
+    message: messageInput.value,
+  };
+  console.log(state);
+  localStorage.removeItem(storageKey);
+  emailInput.value = '';
+  messageInput.value = '';
+};
+
+// Добавляем обработчики событий
+form.addEventListener('input', throttle(saveFormState, 500));
+form.addEventListener('submit', handleSubmit);
+
+// Загружаем состояние формы при загрузке страницы
+window.addEventListener('load', loadFormState);
